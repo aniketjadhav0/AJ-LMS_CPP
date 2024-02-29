bb2cb21bd0d15ad3b3870968602b922881bfedc3
aniketjadhav0 <aniketj078@gmail.com>
2024-03-01 02:57:02 +0530

    footer , header , topbar files add

--- dashboard.php
+++ dashboard.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./assets/css/bootstrap.min.css" rel="stylesheet"/>
-    <title>LMS</title>
+    <title>Dashboard</title>
     <link rel="stylesheet" href="./assets/css/style.css">
 
 </head>
