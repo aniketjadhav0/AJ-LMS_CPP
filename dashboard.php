5a911eb93a8e64535c99406f55882b7d4acc616e
aniketjadhav0 <aniketj078@gmail.com>
2024-03-01 03:09:47 +0530

    .

--- dashboard.php
+++ dashboard.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./assets/css/bootstrap.min.css" rel="stylesheet"/>
-    <title>Dashboard</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
-
-</head>
-
-<body>
+<?php include_once("include/header.php") ?>
 
     <!-- top navbar start -->
 
