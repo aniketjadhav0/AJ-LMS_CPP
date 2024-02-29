258782a4e7f4b6da58cb1f6e94be4c1f79e0b55a
aniketjadhav0 <aniketj078@gmail.com>
2024-03-01 03:13:04 +0530

    footer added

--- dashboard.php
+++ dashboard.php
@@ -513,11 +513,4 @@
     <!-- Main content end -->
 
 
-    <script src="assets\js\bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
-    <script src="assets\js\17903776.js" crossorigin="anonymous"></script>
-
-</body>
-
-</html>
+    <?php include_once("include/header.php") ?>
---
+++ include/footer.php
@@ -0,0 +1,8 @@
+<script src="assets\js\bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+    <script src="assets\js\17903776.js" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
