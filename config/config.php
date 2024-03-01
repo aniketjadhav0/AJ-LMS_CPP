358e6d64ed0709d8c5bf4b598f4a50cab8f30eea
aniketjadhav0 <aniketj078@gmail.com>
2024-03-01 10:48:59 +0530

    HTML TO PHP CONVERSION COMPLITE

--- config/config.php
+++ config/config.php
@@ -1,6 +1,17 @@
 <?php
 
-define("BASE_URL","http://localhost/AJ-LMS-CPP/");
+if($_SERVER['HTTP_HOST'] == 'localhost'){
+
+    define("BASE_URL","http://localhost/AJ-LMS-CPP/");
 define("DIR_URL",$_SERVER["DOCUMENT_ROOT"] . "/AJ-LMS-CPP/");
+
+}
+
+else{
+    define("BASE_URL","http://lms.com");
+define("DIR_URL",$_SERVER["DOCUMENT_ROOT"]);
+}
+
+
 
 ?>
